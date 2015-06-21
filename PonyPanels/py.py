f6fca8288be562d543e573696f55c2a58c4c7030
Niki .V <conik24@gmail.com>
2015-06-21 19:40:24 +0300

    Updating things

--- PonyPanels/py.py
+++ PonyPanels/py.py
@@ -2,8 +2,7 @@ import requests
 file = open("paths.txt", "r")
 url = raw_input("Enter URL: ")
 for line in file.readlines():
-	url2 = url + line
-	if(requests.get(url2.rstrip()).status_code == 404):
+	if(requests.get(url + line.rstrip()).status_code == 404):
 		print "Failed: " + str(requests.get(url + line).status_code) + " / " + url + line
 	else:
 		print line + "=" + "Nailed"
